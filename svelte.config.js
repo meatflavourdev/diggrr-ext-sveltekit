271c1546880605714070feeb39679f4f871410da
Jeremy Dombrowski <jeremy@meatflavour.dev>
2022-06-24 15:54:26 -0700

    sveltekit browser extension init

---
+++ svelte.config.js
@@ -0,0 +1,25 @@
+import adapter from 'sveltekit-adapter-chrome-extension';
+import preprocess from 'svelte-preprocess';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
+	kit: {
+		adapter: adapter("")
+	}
+};
+
+export default {
+	kit: {
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
+		})
+	}
+};
